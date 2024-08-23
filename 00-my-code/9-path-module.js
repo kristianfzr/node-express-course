2c9ea7bd9ea9df423ee87d40ab01f345fc0d6e9e
Kristian Kanchev <kristianfzr@gmail.com>
2024-08-23 13:41:43 +0000

    added os and path modules

---
+++ 00-my-code/9-path-module.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+console.log(path.sep);
+
+const filePath = path.join('/content', 'generic-stuff', 'test.txt');
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+const absolute = path.resolve(__dirname, 'content', 'generic-stuff', 'test.txt');
+console.log(absolute);
