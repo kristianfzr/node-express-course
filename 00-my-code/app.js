870f6176e688984622eb1113d0a403a67e7286bb
Kristian Kanchev <kristianfzr@gmail.com>
2024-08-23 13:12:04 +0000

    Added flavors and mind grenade

--- 00-my-code/app.js
+++ 00-my-code/app.js
@@ -1,8 +1,8 @@
 const names = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenedate');
 
 sayHi('susan');
 sayHi(names.john);
 sayHi(names.peter);
-
-// comment test
