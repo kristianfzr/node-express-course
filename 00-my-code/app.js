81b49b5c6220b65f548d9c2e7598be5a4fd69b85
Kristian Kanchev <kristianfzr@gmail.com>
2024-08-24 15:40:47 +0000

    add fs-async

--- 00-my-code/app.js
+++
@@ -1,9 +0,0 @@
-const { readFile, writeFileSync } = require('fs');
-
-readFile('./content/first.txt', 'utf8', (err, result) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(result);
-});
